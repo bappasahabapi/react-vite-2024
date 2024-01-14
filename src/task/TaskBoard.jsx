5e989d17b1aba16475e82b2f4a66d9f8c8a21d41
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2024-01-14 21:17:25 +0600

    list part done

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+
+    //json
+    const defaultTask ={
+        "id":crypto.randomUUID(),
+        "title": "This is test title one",
+        "description":"There is a dreamy boy whose name is Bappa There is a dreamy boy whose name is Bappa There is a dreamy boy whose name is Bappa",
+        "tags":["web","js"],
+        "priority":"High",
+        "isFavorite":true
+    }
+
+    const [tasks, setTasks]=useState([defaultTask])
   return (
     <div>
       <section className="mb-20" id="tasks">
         <div className="container">
-            <SearchTask/>
-
+          <SearchTask />
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskActions/>
-            <TaskList/>
+            <TaskActions />
+            <TaskList tasks={tasks}/>
           </div>
         </div>
       </section>
