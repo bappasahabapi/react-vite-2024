b2951051458533c66748b1027d48904ddcde2414
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2024-01-16 20:53:29 +0600

    update search

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,30 +1,30 @@
 /* eslint-disable react/prop-types */
 
-export const Book = ({ book }) => {
+export const Book = ({ book, onFav }) => {
+  // console.log(book)
+  const { id, image, title, rating, author, price,isFavorite } = book;
   return (
-    <div key={book.id} className="space-y-3">
+    <div key={id} className="space-y-3">
       <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4">
-        <img className="max-w-[144px]" src={book.image} alt={book.title} />
+        <img className="max-w-[144px]" src={image} alt={title} />
       </div>
       <div className="space-y-3">
-        <h4 className="text-lg font-bold lg:text-xl">{book.title}</h4>
+        <h4 className="text-lg font-bold lg:text-xl">{title}</h4>
         <p className="text-xs lg:text-sm">
-          By: <span>{book.author}</span>
+          By: <span>{author}</span>
         </p>
-        <div className="flex items-center justify-between">
-          <h4 className="text-lg font-bold lg:text-xl">${book.price}</h4>
-          {/* Stars */}
+        <div className="flex items-center justify-between ">
+          <h4 className="text-lg font-bold lg:text-xl">${price}</h4>
           <div className="flex items-center space-x-1">
-            {[...Array(book.rating).keys()].map((_, index) => (
+            {Array.from({ length: book.rating }, (_, index) => (
               <img
                 key={index}
                 src="/src/assets/star.svg"
                 alt={`Star ${index + 1}`}
               />
             ))}
-            <span className="text-xs lg:text-sm">({book.rating} Star)</span>
+            <span className="text-xs lg:text-sm">({rating} Star)</span>
           </div>
-          {/* Stars end */}
         </div>
         <div className="flex items-center gap-3 text-xs lg:text-sm">
           <button className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336] py-1.5 text-white transition-all hover:opacity-80 lg:py-1.5">
@@ -40,17 +40,41 @@ export const Book = ({ book }) => {
             </svg>
             Add to Cart
           </button>
-          <button 
-          className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#DC2954]/[14%] py-1.5 text-[#DC2954] transition-all hover:bg-[#DC2954]/[24%] lg:py-1.5">
-            <svg
-              width="16"
-              height="14"
-              viewBox="0 0 21 19"
-              fill="none"
+          <button
+            onClick={() => onFav(id)}
+            className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#DC2954]/[14%] py-1.5 text-[#DC2954] transition-all hover:bg-[#DC2954]/[24%] lg:py-1.5"
+          >
+            {isFavorite ? (
+              <svg
               xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-5 w-5 text-red-600"
             >
-            </svg>
-            Favourite
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg> ): (
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5 text-red-600"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+            )}
+            Favorite
           </button>
         </div>
       </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,94 @@
+import { useState } from "react";
 import BooksContainer from "./BooksContainer";
 import Filters from "./Filters";
 import SearchAction from "./SearchAction";
 
 export default function Header() {
+  const booksData = [
+    {
+      id: 1,
+      title: "JavaScript and JQuery",
+      author: "Jon Duckett",
+      price: 62,
+      rating: 4,
+      image: "/src/assets/book.png",
+      isFavorite: false,
+    },
+    {
+      id: 2,
+      title: "Python",
+      author: "Jon Duckett",
+      price: 62,
+      rating: 5,
+      image: "/src/assets/book.png",
+      isFavorite: true,
+    },
+    {
+      id: 3,
+      title: "python",
+      author: "Jon Duckett",
+      price: 62,
+      rating: 2,
+      image: "/src/assets/book.png",
+      isFavorite: false,
+    },
+    {
+      id: 4,
+      title: "JavaScript and JQuery",
+      author: "Jon Duckett",
+      price: 62,
+      rating: 4,
+      image: "/src/assets/book.png",
+      isFavorite: true,
+    },
+    {
+      id: 5,
+      title: "JavaScript and JQuery",
+      author: "Jon Duckett",
+      price: 62,
+      rating: 4,
+      image: "/src/assets/book.png",
+      isFavorite: false,
+    },
+
+    // Add more books as needed
+  ];
+
+  const [books, setBooks] = useState(booksData);
+
+  function handleFavorite(bookId) {
+    // console.log('clicked',bookId)
+    setBooks(
+      books.map((book) => {
+        if (book.id === bookId) {
+          return {
+            ...book,
+            isFavorite: !book.isFavorite,
+          };
+        } else {
+          return book;
+        }
+      })
+    );
+  }
+
+  function handleSearch(searchText) {
+    // console.log(searchText)
+    const filterSearch = booksData.filter((book) =>
+      book.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+    );
+    setBooks(filterSearch)
+  }
+
   return (
-    <main className="my-10 lg:my-14 bg-orange-200">
+    <main className="my-10 lg:my-14">
       <header className="mb-8 lg:mb-10 mx-auto max-w-7xl">
         <div className="mx-auto flex items-end justify-between max-md:max-w-[95%] max-md:flex-col max-md:items-start max-md:space-y-4">
-          <SearchAction />
+          <SearchAction onSearch={handleSearch} />
           <Filters />
         </div>
       </header>
-      <BooksContainer />
+      <BooksContainer books={books} onFav={handleFavorite} />
     </main>
   );
 }
