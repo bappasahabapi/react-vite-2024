1b1fe188d44f68948cd7939f15e051ccac0d96c5
Bappa Saha <bappasaha161@gmail.com>
2024-04-04 15:41:23 +0600

    Basic-layout formed

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { getDocuments } from "./lib/doc";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,13 +10,27 @@ export const metadata = {
   description: "A documentary website for readme files.",
 };
 
+// here the children are page props.
 export default function RootLayout({ children }) {
-
-  const allDocuments= getDocuments();
-  console.log(allDocuments)
+  const allDocuments = getDocuments();
+  console.log(allDocuments);
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="h-full lg:ml-72 xl:ml-80">
+          <Header />
+          <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+            <main className="flex-auto py-16">
+              <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
+                <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
+                  <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"></div>
+                </div>
+              </div>
+              {children}
+            </main>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
