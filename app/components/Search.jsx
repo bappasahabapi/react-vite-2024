82ac0ffa20627e50272d279d5728392bc0e8215a
Bappa Saha <bappasaha161@gmail.com>
2024-04-05 23:58:45 +0600

    dynamic route workable

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,9 +1,74 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useDebounce } from "@/hooks/useDebounce";
+import SearchResult from "./SearchResult";
+import { useRouter } from "next/navigation";
+
+const Search = ({ docs }) => {
+  const [searchResult, setSearchResult] = useState([]);
+  const [term, setTerm] = useState("");
+  const router =useRouter();
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setTerm(value);
+
+    doSearch(value);
+  }
 
 
-const Search = () => {
+  // console.log(docs)
+  const doSearch = useDebounce((term) => {
+    const found = docs.filter((doc) => {
+      return doc.title.toLowerCase().includes(term.toLowerCase());
+    });
+    console.log(found);
+    setSearchResult(found);
+  }, 3000);
+
+
+  function handleCloseSearchResults(e){
+    e.prevent.default();
+    router.push(e.target.href)
+    setTerm("")
+  }
+
   return (
-    <div>Search</div>
-  )
-}
+    <>
+      <div className="lg:block lg:max-w-md lg:flex-auto">
+        <button
+          type="button"
+          className="focus:[&amp;:not(:focus-visible)]:outline-none hidden h-8 w-full items-center gap-2 rounded-full bg-white pl-2 pr-3 text-sm text-zinc-500 ring-1 ring-zinc-900/10 transition hover:ring-zinc-900/20 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:ring-white/20 lg:flex"
+        >
+          <Image
+            src="/search.svg"
+            alt="Search"
+            className="h-5 w-5"
+            width={50}
+            height={50}
+          />
+          <input
+            type="text"
+            value={term}
+            placeholder="Search..."
+            onChange={handleChange}
+            className="flex-1 focus:border-none focus:outline-none"
+          />
+        </button>
+      </div>
+      {term && term.trim().length > 0 && (
+        <SearchResult
+          results={searchResult}
+          term={term}
+          closeSearchResults={handleCloseSearchResults}
+        />
+      )
+      
+      }
+    </>
+  );
+};
 
-export default Search
+export default Search;
