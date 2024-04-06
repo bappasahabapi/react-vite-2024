2b4c69f983fe542a71856a9fc4a38d6b1a1919d4
Bappa Saha <bappasaha161@gmail.com>
2024-04-06 12:49:25 +0600

    4-difining routes

--- README.md
+++ README.md
@@ -11,6 +11,17 @@ yarn dev
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 
-4. [Defining Routes](https://nextjs.org/docs/app/building-your-application/routing/defining-routes)
+4. [**Defining Routes**](https://nextjs.org/docs/app/building-your-application/routing/defining-routes)
+
+    - Next.js uses a **file-system based router** where **folders are used to define routes.**
+  
+
+      ![GitHub Web](https://nextjs.org/_next/image?url=%2Fdocs%2Flight%2Froute-segments-to-path-segments.png&w=3840&q=75)
+    
+    - A special **page.js** file is used to make route segments publicly accessible.
+     ![GitHub Web](https://nextjs.org/_next/image?url=%2Fdocs%2Flight%2Fdefining-routes.png&w=3840&q=75)
+
+5. [**Pages and Layouts**](https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts)
+
 
 
---
+++ app/dashboard/settings/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SettingPage = () => {
+  return (
+    <div> 
+
+    <h1 className='text-4xl '>Settings page </h1>
+    <a className='text-blue-300 ' href='http://localhost:3000/dashboard/settings'>Home Page</a>
+    <p className="mt-10">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora dicta nostrum accusantium voluptates ullam magni voluptatem, vel atque. Maxime totam cum, recusandae reprehenderit dolorum ab itaque nisi, accusamus voluptatum dicta, odio iure rerum? Minus dolores dolor accusamus fuga, assumenda blanditiis obcaecati, sed cum, nobis beatae odit! Quod, iusto nisi ratione fuga, molestiae delectus quas a itaque debitis facilis libero magni aliquam assumenda consectetur tempora incidunt expedita aut saepe rerum? Mollitia sapiente natus cumque repellat quos velit? Ab corrupti molestiae totam dolore nihil libero dolorum praesentium nemo fugiat velit quis molestias, cum explicabo culpa, iure vitae dolorem, maiores excepturi necessitatibus itaque.
+    </p>
+    </div>
+  )
+}
+
+export default SettingPage
