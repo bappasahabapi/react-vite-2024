3ba48d622a07bc412c252aeb0e49eabe6423c2a1
Bappa Saha <bappasaha161@gmail.com>
2024-04-12 16:00:36 +0600

    setup project

--- app/cashboard/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET(){
-  console.log("Rendering from server...");
-  return new Response("welcome from route.js file "
-  )
-}
--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
+
 
 export default function Home() {
   return (
    <div>
     <h1 className="text-2xl">
-      Hit the url <b><Link href={`http://localhost:3000/api/comments`}>Click to show API  Json Response For Comments</Link></b>
+      Photo Gallary
     </h1>
-    <Link className='text-2xl bg-indigo-200' href="/jfldfjl">Click here to go not found page</Link>
-    <Link className='text-2xl bg-indigo-200' href="/product/100/review/5">Click here to go http://localhost:3000/product/100/review/5</Link>
-    <Link className='text-2xl bg-indigo-200' href="/contact">Contact-Interationalization</Link>
+    
    </div>
   );
 }
