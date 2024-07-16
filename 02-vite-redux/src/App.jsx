96f4752117329e4f68e0fa8c9cd3e711d296ab14
Bappa Saha <bappasaha161@gmail.com>
2024-07-17 00:05:58 +0600

    Update

--- 02-vite-redux/src/App.jsx
+++ 02-vite-redux/src/App.jsx
@@ -2,11 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useEffect } from "react";
-import { calculateTotal } from "./features/cart/cartSlice";
+import { calculateTotal, getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
 
   const { isOpen } = useSelector((store) => store.modal);
@@ -14,6 +14,22 @@ function App() {
   useEffect(() => {
     dispatch(calculateTotal());
   }, [cartItems, dispatch]);
+
+// when application load 
+  useEffect(()=>{
+    dispatch(getCartItems())
+  },[])
+
+  if(isLoading){
+    return(
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+
+
   return (
     <main>
       {isOpen && <Modal />}
--- 02-vite-redux/src/features/cart/cartSlice.js
+++ 02-vite-redux/src/features/cart/cartSlice.js
@@ -1,17 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+// import { openModal } from "../modal/modalSlice";
+// import cartItems from "../../cartItems";
+
+const url = "https://www.course-api.com/react-useReducer-cart-project";
 
 const initialState = {
   // cartItems:[],
-  cartItems: cartItems,
+  // cartItems: cartItems,
+  cartItems: [], //from url
   amount: 1,
   totalAmount: 0,
   isLoading: true,
 };
 
+//todo:1st url data fetching using fetch()
+// export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+//   return fetch(url)
+//     .then((resp) => resp.json())
+//     .catch((error) => console.log(error));
+// });
+
+//todo:2nd url data fetching using axios()
+export const getCartItems = createAsyncThunk("cart/getCartItems", async(name,thunkAPI) => {
+  try {
+    // console.log(name)
+    // console.log(thunkAPI)
+    // console.log(thunkAPI.getState())
+    // console.log(thunkAPI.dispatch(openModal()))
+    const response =await axios(url);
+    // console.log(response)
+    return response.data;
+
+  } catch (error) {
+    return thunkAPI.rejectWithValue('Something went wrong')
+  }
+});
+
+
+
+
+
+
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  //Slice part is handle here
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
@@ -42,6 +78,42 @@ const cartSlice = createSlice({
       state.totalAmount = totalAmount;
     },
   },
+    //todo:best way builder call back notation
+    extraReducers:(builder)=>{
+      builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
+
+
+
+  //todo url fetching [fetch and axios use] is handleing in exrtra reducers [API part]
+  // extraReducers:{
+  //   [getCartItems.pending]:(state)=>{
+  //     state.isLoading=true;
+  //   },
+  //   [getCartItems.fulfilled]:(state,action)=>{
+  //     // console.log(action.payload[0].id);
+      
+  //     state.isLoading=false;
+  //     state.cartItems=action.payload;
+  //   },
+  //   [getCartItems.rejected]:(state)=>{
+  //     state.isLoading=false;
+  //   },
+  // }
+
+
+  }
 });
 
 // console.log(cartSlice) // cart:{actions:}
